2b2fa53ac2a0695228dd381da8a71c59eee132ed
Nataliya <nataliya.laz@gmail.com>
2022-09-01 14:00:27 +0300

    add thumbnails

--- emojiArtDoc/EmojiArtDocument.swift
+++ emojiArtDoc/EmojiArtDocument.swift
@@ -11,6 +11,8 @@ class emojiArtDocument: UIDocument {
     
     var emojiArt: EmojiArt?
     
+    var thumbnail: UIImage?
+    
     override func contents(forType typeName: String) throws -> Any {
         return emojiArt?.json ?? Data()// return json or blank document
     }
@@ -19,7 +21,14 @@ class emojiArtDocument: UIDocument {
         if let json = contents as? Data {
             emojiArt = EmojiArt(json: json)
         }
-   
+    }
+    
+    override func fileAttributesToWrite(to url: URL, for saveOperation: UIDocument.SaveOperation) throws -> [AnyHashable : Any] {
+        var attributes = try super.fileAttributesToWrite(to: url, for: saveOperation)
+        if let thumbnail = self.thumbnail {
+            attributes[URLResourceKey.thumbnailDictionaryKey] = [URLThumbnailDictionaryItem.NSThumbnail1024x1024SizeKey: thumbnail]
+        }
+        return attributes
     }
 }
 
